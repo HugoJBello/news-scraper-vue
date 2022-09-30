f62469e794797db36bb29e1be902cb5846dc0a22
Hugo J. Bello <hjbello.wk@protonmail.com>
2022-09-30 09:22:22 +0200

    clean

--- src/services/newsInDayFilterService.ts
+++ src/services/newsInDayFilterService.ts
@@ -3,8 +3,25 @@ import type { NewScrapedI } from "@/models/NewScraped";
 
 export const filterLastIterationOnly = (news: NewScrapedI[]): NewScrapedI[] => {
    const iterations = news.map(item => item.scrapingIteration)
-   console.log(iterations)
-   const max = Math.max.apply(null, iterations)
-   return news.filter(item => item.scrapingIteration === max)
+   const newsCopy = [... news];
+   
+   newsCopy.sort(function(a,b){
+      return (a.scrapedAt as any) - ( b.scrapedAt as any);
+    });
+
+   const lastScraped = newsCopy[newsCopy.length -1] 
+   //const max = Math.max.apply(null, iterations)
+   const max = lastScraped.scrapingIteration
+   console.log(max, lastScraped.scrapedAt)
+   const newsFiltered =  news.filter(item => (item.scrapingIteration === max || item.scrapingIteration=== max-1))
+   newsFiltered.sort((a,b)=>{
+      return (a.newsIndex as any) - ( b.newsIndex as any);
+
+   })
+   newsFiltered.sort((a,b)=>{
+      return (a.scrapingIteration as any) - ( b.scrapingIteration as any);
+
+   })
+   return newsFiltered
 };
  
